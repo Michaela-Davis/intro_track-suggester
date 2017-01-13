aed1eedf98ce6c983f69b6c99fcce637d4c1910a
Michaela Davis <michaela.delight@gmail.com>
2017-01-13 12:07:37 -0800

    add event to form submit in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,10 @@ $(document).ready(function() {
   // Hide all answer cards on page load
   $(".answer-card").hide();
 
+  // Add event to form submit
+  $("form").submit(function(event){
+
+
+    event.preventDefault();
+  });
 });
