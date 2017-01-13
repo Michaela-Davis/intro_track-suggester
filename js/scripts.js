162bfbf55d0d2eab74e7736b28c3bc172f8d6893
Michaela Davis <michaela.delight@gmail.com>
2017-01-13 14:55:36 -0800

    finalize JS/jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,21 @@ $(document).ready(function() {
 
   // Add event to form submit
   $("form").submit(function(event) {
-    // alert("got to beginning of form submit")
+    // Define variables
     var age = parseInt($("#age").val());
-    console.log(age);
     var end = $('input.end:checked').val();
+    var microsoft = $('input.microsoft:checked').val();
+    var prefer = $('input.prefer:checked').val();
 
+    // Define branching / logic statements
     if (end === "front") {
       $("#cardCSS").show();
+      $("form").hide();
+    } else if (microsoft === "love") {
+      $("#cardC").show();
+      $("form").hide();
+    } else if (prefer === "interactive") {
+      $("#cardRuby").show();
       $("form").hide();
     } else {
       $("#cardPHP").show();
