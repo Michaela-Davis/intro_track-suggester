b17dc177fb87274823f31b243387feca416591df
Michaela Davis <michaela.delight@gmail.com>
2017-01-13 14:45:27 -0800

    add branching for results

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,21 @@ $(document).ready(function() {
   $(".answer-card").hide();
 
   // Add event to form submit
-  $("form").submit(function(event){
+  $("form").submit(function(event) {
+    // alert("got to beginning of form submit")
+    var age = parseInt($("#age").val());
+    console.log(age);
+    var end = $('input.end:checked').val();
 
+    if (end === "front") {
+      $("#cardCSS").show();
+      $("form").hide();
+    } else {
+      $("#cardPHP").show();
+      $("form").hide();
+    }
 
     event.preventDefault();
+    // alert("got to end of form submit")
   });
 });
